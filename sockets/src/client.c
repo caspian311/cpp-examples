ab0f4fa58a605ac3b384eac3d1d421f368a3fd96
Matt Todd <matt.c.todd@gmail.com>
2015-05-09 10:49:11 -0500

    more refactorings on the client

--- sockets/src/client.c
+++ sockets/src/client.c
@@ -27,53 +27,68 @@ int get_port(char *argv[])
    return atoi(argv[2]);
 }
 
-int main(int argc, char *argv[])
+int connect_to_server(struct hostent *server, int port)
 {
-   validate_input(argc, argv);
-
-   int port = get_port(argv);
-
-   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-   if (sockfd < 0) 
+   int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+   if (socket_fd < 0) 
    {
       error("ERROR opening socket");
    }
-   struct hostent *server;
-   server = gethostbyname(argv[1]);
+   struct sockaddr_in server_address;
+   bzero((char *) &server_address, sizeof(server_address));
+   server_address.sin_family = AF_INET;
+   bcopy((char *)server->h_addr, (char *)&server_address.sin_addr.s_addr, server->h_length);
+   server_address.sin_port = htons(port);
+   int connection = connect(socket_fd, (struct sockaddr *) &server_address, sizeof(server_address));
+   if (connection < 0)
+   {
+      error("ERROR connecting");
+   }
+
+   return socket_fd;
+}
+
+struct hostent *get_server(char *argv[])
+{
+   struct hostent *server = gethostbyname(argv[1]);
    if (server == NULL) 
    {
       fprintf(stderr,"ERROR, no such host\n");
       exit(0);
    }
-   struct sockaddr_in serv_addr;
-   bzero((char *) &serv_addr, sizeof(serv_addr));
-   serv_addr.sin_family = AF_INET;
-   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-   serv_addr.sin_port = htons(port);
-   if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-   {
-      error("ERROR connecting");
-   }
+
+   return server;
+}
+
+int main(int argc, char *argv[])
+{
+   validate_input(argc, argv);
+
+   int port = get_port(argv);
+   struct hostent *server = get_server(argv);
+
+   int socket_fd = connect_to_server(server, port);
 
    printf("Please enter the message: ");
 
    char buffer[256];
    bzero(buffer, 256);
    fgets(buffer, 255, stdin);
-   int bytes_written_to_server = write(sockfd, buffer, strlen(buffer));
+
+   int bytes_written_to_server = write(socket_fd, buffer, strlen(buffer));
    if (bytes_written_to_server < 0)
    {
       error("ERROR writing to socket");
    }
    bzero(buffer, 256);
-   int bytes_from_response = read(sockfd, buffer, 255);
+   int bytes_from_response = read(socket_fd, buffer, 255);
    if (bytes_from_response < 0)
    {
       error("ERROR reading from socket");
    }
    printf("%s\n", buffer);
 
-   close(sockfd);
+   close(socket_fd);
 
    return 0;
 }
