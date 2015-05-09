e0cc31e0ebcbab723f3eb551d6db4a987a893dfe
Matt Todd <matt.c.todd@gmail.com>
2015-05-09 10:23:38 -0500

    refactored server

--- sockets/src/server.c
+++ sockets/src/server.c
@@ -14,42 +14,87 @@ void error(const char *msg)
     exit(1);
 }
 
+int get_port(int argc, char *argv[])
+{
+   if (argc < 2) 
+   {
+      fprintf(stderr,"ERROR, no port provided\n");
+      exit(1);
+   }
+
+   return atoi(argv[1]);
+}
+
+int bind_to_port(int port) 
+{
+   int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+   if (server_socket < 0)
+   {
+      error("ERROR opening socket");
+   }
+
+   struct sockaddr_in server_address;
+   bzero((char *) &server_address, sizeof(server_address));
+   server_address.sin_family = AF_INET;
+   server_address.sin_addr.s_addr = INADDR_ANY;
+   server_address.sin_port = htons(port);
+   if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) 
+   {
+      error("ERROR on binding");
+   }
+
+   listen(server_socket, 5);
+
+   return server_socket;
+}
+
+int wait_for_client(int server_socket)
+{
+   struct sockaddr_in client_address;
+   socklen_t client_address_length;
+   client_address_length = sizeof(client_address);
+   int socket_to_client = accept(server_socket, (struct sockaddr *) &client_address, &client_address_length);
+   if (socket_to_client < 0) 
+   {
+      error("ERROR on accept");
+   }
+
+   return socket_to_client;
+}
+
+void read_data_from_client(int socket_to_client, char* buffer)
+{
+   bzero(buffer, 256);
+   int bytesReadFromClient = read(socket_to_client, buffer, 255);
+   if (bytesReadFromClient < 0) 
+   {
+      error("ERROR reading from socket");
+   }
+}
+
+void send_response_to_client(int socket_to_client)
+{
+   int bytesSentBack = write(socket_to_client, "I got your message", 18);
+   if (bytesSentBack < 0) 
+   {
+      error("ERROR writing to socket");
+   }
+}
+
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
+   int port = get_port(argc, argv);
+   int server_socket = bind_to_port(port);
+   int socket_to_client = wait_for_client(server_socket);
+
+   char buffer[256];
+   read_data_from_client(socket_to_client, buffer);
+   printf("Message from client: %s\n", buffer);
+
+   send_response_to_client(socket_to_client);
+
+   close(socket_to_client);
+   close(server_socket);
+
+   return 0; 
 }
