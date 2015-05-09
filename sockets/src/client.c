a6f92bd423b2456610e6c5b92de174c900572a4e
Matt Todd <matt.c.todd@gmail.com>
2015-05-09 10:39:18 -0500

    refactored the client a bit

--- sockets/src/client.c
+++ sockets/src/client.c
@@ -13,45 +13,67 @@ void error(const char *msg)
     exit(0);
 }
 
+void validate_input(int argc, char *argv[])
+{
+   if (argc < 3) 
+   {
+      fprintf(stderr,"usage %s hostname port\n", argv[0]);
+      exit(0);
+   }
+}
+
+int get_port(char *argv[])
+{
+   return atoi(argv[2]);
+}
+
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+   validate_input(argc, argv);
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);
-    return 0;
+   int port = get_port(argv);
+
+   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   if (sockfd < 0) 
+   {
+      error("ERROR opening socket");
+   }
+   struct hostent *server;
+   server = gethostbyname(argv[1]);
+   if (server == NULL) 
+   {
+      fprintf(stderr,"ERROR, no such host\n");
+      exit(0);
+   }
+   struct sockaddr_in serv_addr;
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+   serv_addr.sin_port = htons(port);
+   if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+   {
+      error("ERROR connecting");
+   }
+
+   printf("Please enter the message: ");
+
+   char buffer[256];
+   bzero(buffer, 256);
+   fgets(buffer, 255, stdin);
+   int bytes_written_to_server = write(sockfd, buffer, strlen(buffer));
+   if (bytes_written_to_server < 0)
+   {
+      error("ERROR writing to socket");
+   }
+   bzero(buffer, 256);
+   int bytes_from_response = read(sockfd, buffer, 255);
+   if (bytes_from_response < 0)
+   {
+      error("ERROR reading from socket");
+   }
+   printf("%s\n", buffer);
+
+   close(sockfd);
+
+   return 0;
 }
